3bd24604805ec707941f657e66c5a6dba8005ce8
angela-mladen <121134774+angela-mladen@users.noreply.github.com>
2023-07-05 15:06:46 +0200

    changes in domain

--- Cinema.Domain/SpecialCollectionClass.cs
+++ Cinema.Domain/SpecialCollectionClass.cs
@@ -9,12 +9,12 @@ namespace Cinema.Domain
 {
     public class SpecialCollectionClass
     {
-        // results of search query
+        
         public List<Ticket> SearchResults { get; set; }
 
         public SelectList TypeOptions { get; set; }
 
-        // Selected search options
+       
         [Required]
         public string SearchGenre { get; set; }
         public string CurrentFilter { get; set; }
@@ -22,7 +22,5 @@ namespace Cinema.Domain
         public DateTime? FromDate { get; set; }
         public DateTime? ToDate { get; set; }
     }
-    //class SpecialCollectionClass
-    //{
-    //}
+    
 }
